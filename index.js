ecb46e9c20184c66190f998c62821aedf45695ea
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2019-11-18 22:13:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PREFIX = '~';
 
 bot.on('ready', () =>{
 	console.log('The bot is online!');
-	bot.user.setActivity('To ~help', {type: "LISTENING"});
+	bot.user.setActivity('~help', {type: "LISTENING"});
 });
 
 bot.on('guildMemberAdd', member =>{
