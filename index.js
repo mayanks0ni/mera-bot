3fe4f1bfdd31f358a9cb3788629aff8a6497098d
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2019-11-27 01:16:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -135,27 +135,6 @@ case 'avatar':
        message.delete()
 	message.channel.bulkDelete(args[1]);
 			  
-		  case 'announce':
-			  if(!message.member.hasPermission("MANAGE_MESSAGES") return 
-			     const noperms4 = new Discord.RichEmbed()
-			     .setDescription('You don\'t have enough permissions')
-			     message.channel.sendEmbed(noperms4);
-			  message.delete()
-			  let mChannel = message.mention.channels.first();
-			  let argresult;
-			  if(!mChannel){
-				  argresult = args.slice(1).join(" ")
-				  const noc = new Discord.RichEmbed()
-				  .setDescription(`${argresult}`)
-				  mChannel.sendEmbed(noc);
-			  }
-			  else{
-				  argresult = args.join(" ")
-				  const ch = new Discord.RichEmbed()
-				  .setDescription(`${argresult}`)
-				  message.channel.sendEmbed(ch);
-			  }
-			  
 }});
 
 
