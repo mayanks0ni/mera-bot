7268b1ebd5058051fbfcb2df64bd9255b0ea8749
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2019-12-20 15:47:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -78,6 +78,7 @@ bot.on('message', message=>{
 		  break;
 
 	  case 'kick':
+			  message.delete()
 			if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send('YOU DON\'T HAVE ENOUGH PERMISSIONS!')
 			const user1 = message.mentions.users.first();
 			if (user1) {
@@ -99,6 +100,7 @@ bot.on('message', message=>{
 				break;
 			}
 		case 'ban':
+			  message.delete()
 			if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('YOU DON\'T HAVE ENOUGH PERMISSIONS!')
 			const user = message.mentions.users.first();
 			if (user) {
