ab893619da08bc19ad285c5a4081fd1ffff9de5c
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2019-12-22 00:54:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-const fs = require('fs');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-bot.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	bot.commands.set(command.name, command);
-}
-
-if (!client.commands.has(command)) return;
-
-try {
-	client.commands.get(command).execute(message, args);
-} catch (error) {
-	console.error(error);
-	message.reply('there was an error trying to execute that command!');
-}
 const PREFIX = '+'; 
 
 bot.on('ready', () =>{
