45307a3ac78df568974d553db18048338c4c4cb5
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2019-12-22 00:49:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+bot.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	bot.commands.set(command.name, command);
+}
+
+if (!client.commands.has(command)) return;
+
+try {
+	client.commands.get(command).execute(message, args);
+} catch (error) {
+	console.error(error);
+	message.reply('there was an error trying to execute that command!');
+}
 const PREFIX = '+'; 
 
 bot.on('ready', () =>{
@@ -34,12 +52,7 @@ bot.on('message', message=>{
 	  let args = message.content.substring(PREFIX.length).split(" ");
 
 	  switch(args[0]){
-	  case 'info':
-		  const Infoembed = new Discord.RichEmbed()
-		  .setDescription('I am a bot programmed by SpideY!')
-		  message.channel.sendEmbed(Infoembed);
-                  message.react('😀');
-		  break;
+	  
 		  
 	  case 'helpdm':
 		  const embed = new Discord.RichEmbed()
