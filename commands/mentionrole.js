7066c79908b3ae38cecfde97bd3034749254fca8
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2019-12-30 23:21:05 +0530

    Create mentionrole.js

---
+++ commands/mentionrole.js
@@ -0,0 +1,26 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+			message.delete()
+			if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+				const nomrperms = new Discord.RichEmbed()
+				.setDescription('**YOU DON\'T HAVE ENOUGH PERMISSIONS!!**')
+				.setColor(0xff0000)
+				message.channel.sendEmbed(nomrperms);
+			}else{
+			let rolename;
+			rolename = args.slice(1).join(" ")
+			if(!rolename){
+				const nomenrole = new Discord.RichEmbed()
+				.setDescription('**You didn\'t wrote the roles name!**')
+				.setColor(0xcdacfc)
+				message.channel.sendEmbed(nomenrole);
+			} else{
+			const mentionedRole = message.guild.roles.find(r => r.name === rolename);
+			message.channel.send(`${mentionedRole}`);
+		}	} 
+};
+
+module.exports.help = {
+    name: "mentionrole"
+};
