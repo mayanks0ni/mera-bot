4deada8c8e258c00980a7bff5b47fe27488edeac
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2019-12-30 15:29:41 +0530

    Create dm.js

---
+++ commands/dm.js
@@ -0,0 +1,21 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    
+			if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('❌YOU DON\'T HAVE ENOUGH PERMISSIONS!❌')
+			  const dmtext = args.slice(2).join(" ");
+			const dmuser = message.mentions.users.first()
+			message.delete()
+			if(!dmuser) return;
+			const dmmessage = new Discord.RichEmbed()
+		.setDescription(`${dmtext}`)
+		.setFooter(`From IAT Bot at ${message.createdAt}`)
+		.setColor("RANDOM")
+		dmuser.sendEmbed(dmmessage);
+			
+
+};
+
+module.exports.help = {
+    name: "dm"
+};
