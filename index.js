6d21435e7e441f578b4b317aaa049db445f06bad
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2019-12-30 23:17:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -80,12 +80,6 @@ case 'avatar':
    .setImage(`${user3.displayAvatarURL}`)
    .setColor("RANDOM")
    message.channel.sendEmbed(avatarembed);
-   if(!user3){
-   const userembed = new Discord.RichEmbed ()
-   .setTitle(`User avatar for ${message.author.tag}`)
-   .setImage(`${message.author.displayAvatarURL}`)
-   .setColor("RANDOM")
-   message.channel.sendEmbed(userembed);
    break;
 
 		  case 'del':
