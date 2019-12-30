4ad6987d7555a1b3b43edc6c92c0e7af6e8842b8
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2019-12-30 15:23:14 +0530

    Create announce.js

---
+++ commands/announce.js
@@ -0,0 +1,27 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+                        if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('YOU DON\'T HAVE ENOUGH PERMISSIONS!')
+			let ancmessage;
+			let mchannel = message.mentions.channels.first()
+            message.delete()
+			if(mchannel){
+				 ancmessage = args.slice(2).join(" ");
+				const anc = new Discord.RichEmbed()
+				.setDescription(`${ancmessage}`)
+				.setFooter(`Announced by ${message.author.tag}`)
+				.setColor(0x6bffe1)
+				mchannel.sendEmbed(anc);
+			}else{
+                ancmessage = args.slice(1).join(" ");
+				const anc1 = new Discord.RichEmbed()
+				.setDescription(`${ancmessage}`)
+				.setFooter(`Announced by ${message.author.tag}`)
+				.setColor(0xffc859)
+				message.channel.sendEmbed(anc1);
+			}
+};
+
+module.exports.help = {
+    name: "announce"
+};
