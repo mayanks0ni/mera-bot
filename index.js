5db6870a75a0fe55dc9707875c0406b959482a79
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2019-12-31 13:58:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const fs = require("fs");
-const randomPuppy = require("random-puppy");
+
 const PREFIX = '+'; 
 
 bot.commands = new Discord.Collection();
