bbcd265b91a9484065acdc87f2cbf4fdfebecf8f
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-01-01 14:27:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ fs.readdir("./commands/", (err, files) => {
 
 bot.on('ready', () =>{
 	console.log('The bot is online!');
-	bot.user.setActivity('+HELP', {type:"LISTENING"});
+	bot.user.setActivity('+help', {type:"LISTENING"});
 });
 
 bot.on('guildMemberAdd', member =>{
