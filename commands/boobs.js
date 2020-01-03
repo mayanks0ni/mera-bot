a00f432df09543bd59411ec9fce93f33e65f7a48
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-01-04 02:00:56 +0530

    Create boobs.js

---
+++ commands/boobs.js
@@ -0,0 +1,24 @@
+const Discord = module.require("discord.js");
+const randomPuppy = require("random-puppy");
+
+module.exports.run = async (bot, message, args) => {
+    const subReddits = ["boobs"
+			
+		       ];
+
+			const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+
+
+			const img = await randomPuppy(random);
+			const memeembed = new Discord.RichEmbed()
+				.setColor("RANDOM")
+				.setImage(img)
+				.setTitle('Here's a Boob's Image!')
+			
+
+			message.channel.sendEmbed(memeembed);
+};
+
+module.exports.help = {
+    name: "boobs"
+};
