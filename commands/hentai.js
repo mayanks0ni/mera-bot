60756a026acc236ae5e253de47d591c936afbeff
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-01-06 04:31:16 +0530

    Update hentai.js

--- commands/hentai.js
+++ commands/hentai.js
@@ -1,14 +1,14 @@
 const Discord = module.require("discord.js");
 const superagent = require('superagent')
 module.exports.run = async (bot, message, args) => {
-      if (msg.channel.nsfw === true) {
+      if (message.channel.nsfw === true) {
     superagent.get('https://nekobot.xyz/api/image')
     .query({ type: 'hentai_anal'})
     .end((err, response) => {
-      msg.channel.send({ file: response.body.message });
+      message.channel.send({ file: response.body.message });
     });
   } else {
-    msg.channel.send("This isn't NSFW channel!")
+    message.channel.send("This isn't NSFW channel!")
   }
 };
 
