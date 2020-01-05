1b8524c7b8b881ba19640de328a116c65242e4e1
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-01-05 15:57:53 +0530

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,7 +1,7 @@
 const Discord = module.require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-   const user3 = message.mentions.users.first();
+   const user3 = message.mentions.users.first() || message.author
    const avatarembed = new Discord.RichEmbed()
    .setTitle(`User Avatar For ${user3.tag}`)
    .setImage(`${user3.displayAvatarURL}`)
