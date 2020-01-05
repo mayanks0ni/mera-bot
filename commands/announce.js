32cf6fe72ac6238c9e4b580a21b2d3cb6a7904ed
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-01-05 16:02:24 +0530

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -9,14 +9,16 @@ module.exports.run = async (bot, message, args) => {
 				 ancmessage = args.slice(2).join(" ");
 				const anc = new Discord.RichEmbed()
 				.setDescription(`${ancmessage}`)
-				.setFooter(`IAT Bot at ${message.createdAt}`)
+				.setFooter('IAT Bot')
+                                .setTimestamp()
 				.setColor(0x6bffe1)
 				mchannel.sendEmbed(anc);
 			}else{
                 ancmessage = args.slice(1).join(" ");
 				const anc1 = new Discord.RichEmbed()
 				.setDescription(`${ancmessage}`)
-				.setFooter(`IAT Bot at ${message.createdAt}`)
+				.setFooter('IAT Bot')
+                                .setTimestamp()
 				.setColor(0xffc859)
 				message.channel.sendEmbed(anc1);
 			}
