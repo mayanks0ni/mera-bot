dda4e24435b106e60f0ee88e646ac94e32877a79
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-01-06 20:01:34 +0530

    Create porn.js

---
+++ commands/porn.js
@@ -0,0 +1,29 @@
+const Discord = module.require("discord.js");
+const superagent = require('superagent')
+
+module.exports.run = async (bot, message, args) => {
+    if (message.channel.nsfw === true) {
+    superagent.get('https://nekobot.xyz/api/image')
+    .query({ type: 'pgif'})
+    .end((err, response) => {
+      const porn = new Discord.RichEmbed()
+   .setAuthor(`Porn gif for ${message.author.username}!`)
+   .setImage(response.body.message)
+  .setTimestamp()
+  .setColor("RANDOM")
+  .setFooter('IAT Bot')
+      message.channel.sendEmbed(porn);
+    });
+  } else {
+    const notnsfw = new Discord.RichEmbed()
+    .setTitle('This channel is not a nsfw channel!!')
+    .setTimestamp()
+    .setFooter('IAT Bot')
+    .setColor(0xfa0202)
+    message.channel.sendEmbed(notnsfw);
+  }
+};
+
+module.exports.help = {
+    name: "info"
+};
