2e6c29b0dee24ce1d6fdd836e292f9441d29fdd0
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-01-07 01:37:20 +0530

    Update anal.js

--- commands/anal.js
+++ commands/anal.js
@@ -2,8 +2,9 @@ const Discord = module.require("discord.js");
 const superagent = require('superagent')
 module.exports.run = async (bot, message, args) => {
     if (message.channel.nsfw === true) {
-    superagent.get('https://nekos.life/api/v2/img/anal')
-   
+    let body = await superagent
+    .get('https://nekos.life/api/v2/img/anal')
+      
       message.channel.send({ file: body.url });
    
   } else {
