27572b34fc6f98a13d451fbff0a5e06103a7c255
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-01-06 19:58:40 +0530

    Create ass.js

---
+++ commands/ass.js
@@ -0,0 +1,28 @@
+const Discord = module.require("discord.js");
+const superagent = require('superagent')
+module.exports.run = async (bot, message, args) => {
+    if (message.channel.nsfw === true) {
+    superagent.get('https://nekobot.xyz/api/image')
+    .query({ type: 'ass'})
+    .end((err, response) => {
+      const ass = new Discord.RichEmbed()
+   .setAuthor(`Ass Images for ${message.author.username}!`)
+   .setImage(response.body.message)
+  .setTimestamp()
+  .setColor("RANDOM")
+  .setFooter('IAT Bot')
+      message.channel.sendEmbed(ass);
+    });
+  } else {
+    const notnsfw = new Discord.RichEmbed()
+    .setTitle('This channel is not a nsfw channel!!')
+    .setTimestamp()
+    .setColor(0xfa0202)
+    .setFooter('IAT Bot')
+    message.channel.sendEmbed(notnsfw);
+  }
+};
+
+module.exports.help = {
+    name: "ass"
+};
