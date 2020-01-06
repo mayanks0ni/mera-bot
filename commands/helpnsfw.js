9e22ef51425b873f8e718231fbf39db3a6ca0cbd
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-01-06 20:09:19 +0530

    Update helpnsfw.js

--- commands/helpnsfw.js
+++ commands/helpnsfw.js
@@ -3,8 +3,8 @@ const Discord = module.require("discord.js");
 module.exports.run = async (bot, message, args) => {
     const nsfwembed = new Discord.RichEmbed()
 	.setTitle('NSFW Commands!')
-        .addField('porn', 'Command for viewing porn gifs!)
-        .addField('4k', 'Command for viewing 4k porn images!')
+        .addField('porn', 'Command for viewing porn gifs!')
+        .addField('\4k', 'Command for viewing 4k porn images!')
         .addField('pussy', 'Command for viewing pussy images!')
         .addField('hentai', 'Command for viewing Hentai images!)
 	.setColor("GOLD")
