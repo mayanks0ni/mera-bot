0fd7327d7c4c9b909cd5370c0c23537f7f9efc60
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-01-07 00:44:48 +0530

    Create anal.js

---
+++ commands/anal.js
@@ -0,0 +1,17 @@
+const Discord = module.require("discord.js");
+const superagent = require('superagent')
+module.exports.run = async (bot, message, args) => {
+    if (message.channel.nsfw === true) {
+    superagent.get('https://nekos.life/api/v2/img')
+    .query({ type: 'anal'})
+    .end((err, response) => {
+      message.channel.send({ file: response.body.message });
+    });
+  } else {
+    message.channel.send("This isn't NSFW channel!")
+  }
+};
+
+module.exports.help = {
+    name: "anal"
+};
