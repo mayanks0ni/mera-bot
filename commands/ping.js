d82d73abbc03fd9c7c4be5721491c4f492d4bc63
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-02-04 23:36:27 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const Discord = module.require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     const pingMsg = await message.channel.send('🔄 | Pinging...');
-        return pingMsg.edit(`🐱 | \`(${pingMsg.createdTimestamp - message.createdTimestamp}ms)\``);
+        return pingMsg.edit(`📡Ping **${pingMsg.createdTimestamp - message.createdTimestamp}ms**📡`);
 };
 
 module.exports.help = {
