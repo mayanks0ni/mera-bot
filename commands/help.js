0b661ce5b780ef572f37ab533aaba384a1c7e3de
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-02-09 12:59:10 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -15,20 +15,6 @@ module.exports.run = async (bot, message, args) => {
 		  .addField('mentionrole [rolename]', 'Mentions the role by specifying only its name!')
       .setColor(0xff47bf)
        message.channel.sendEmbed(embed1);
-    
-      if(args[1] === "nsfw"){
-const nsfwembed = new Discord.RichEmbed()
-	.setTitle('NSFW Commands!')
-        .addField('porn', 'Command for viewing porn gifs!')
-        .addField('4k', 'Command for viewing 4k porn images!')
-        .addField('pussy', 'Command for viewing pussy images!')
-        .addField('hentai', 'Command for viewing Hentai images!')
-        .addField('hass', 'Command to view Hentai Ass Images!')
-        .addField('anal', 'Command to view Anal Images!')
-        .addField('hanal', 'Command to view Hentai Anal Images!')
-	.setColor("GOLD")
-        .setTimestamp()		
-message.channel.sendEmbed(nsfwembed);
 };
 
 module.exports.help = {
