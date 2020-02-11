9ca47d20c0825b86b48d57b036c7569db4d660e9
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-02-11 23:16:40 +0530

    Update urban.js

--- commands/urban.js
+++ commands/urban.js
@@ -3,7 +3,7 @@ const urban = require("urban");
 
 module.exports.run = async (bot, message, args) => {
     if(args.length < 1) return message.reply("Please enter something!");
-    let XD = args.slice(1).join(" "); 
+    let XD = args.join(" "); 
 
     urban(XD).first(json => {
         if(!json) return message.reply("No results found for the word!")
