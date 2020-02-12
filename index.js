50c21e61f37cbc49df8aea258fb38d9959b0404e
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-02-12 15:03:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const fs = require("fs");
-let xp = require("./xp.json");
+const SQLite = require("better-sqlite3");
+const sql = new SQLite('./scores.sqlite');
+
 const PREFIX = '+'; 
 
 bot.commands = new Discord.Collection();
@@ -29,6 +31,21 @@ fs.readdir("./commands/", (err, files) => {
 bot.on('ready', () =>{
 	console.log('The bot is online!');
 	bot.user.setActivity('+help', {type:"LISTENING"});
+	
+	const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
+  if (!table['count(*)']) {
+   
+    sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER, level INTEGER);").run();
+    
+    sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
+    sql.pragma("synchronous = 1");
+    sql.pragma("journal_mode = wal");
+  }
+
+  // And then we have two prepared statements to get and set the score data.
+  client.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
+  client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level) VALUES (@id, @user, @guild, @points, @level);");
+	
 });
 
 bot.on('guildMemberAdd', member =>{
@@ -53,11 +70,31 @@ bot.on('guildMemberRemove', member =>{
 })
 
 bot.on('message',async message=>{
-	
+	if(message.author.bot) return;
+	let score;
+  if (message.guild) {
+    score = bot.getScore.get(message.author.id, message.guild.id);
+    if (!score) {
+      score = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, points: 0, level: 1 }
+    }
+    score.points++;
+    const curLevel = Math.floor(0.1 * Math.sqrt(score.points));
+    if(score.level < curLevel) {
+      score.level++;
+	    const levelupembed = new Discord.RichEmbed()
+	    .setAuthor(`${message.author.username} has leveled up!`, `${message.author.displayAvatarURL}`)
+	    .setDescription(`You've leveled up to level **${curLevel}**!`)
+	    .setTimestamp()
+	    .setFooter('IAT Bot')
+      message.channel.send(levelupembed);
+    }
+    bot.setScore.run(score);
+  }
+  
         let msgArray = message.content.split(/\s+/g)
         let command = msgArray[0];
 	let args = message.content.substring(PREFIX.length).split(" ");
-	if (!message.content.startsWith(PREFIX)|| message.author.bot) return;
+	if (!message.content.startsWith(PREFIX)) return;
         if(message.channel.type === "dm") return; 
 	let cmd = bot.commands.get(command.slice(PREFIX.length));
 		if (cmd) cmd.run(bot, message, args);
