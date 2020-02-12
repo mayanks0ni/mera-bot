cbaa25f09ab052d6262d87d7ea8e086b8cc2fe94
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-02-12 19:09:20 +0530

    Create kiss.js

---
+++ commands/kiss.js
@@ -0,0 +1,30 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let men2 = message.mentions.users.first();
+	 if(!men2){
+		 const nomen1 = new Discord.RichEmbed()
+		 .setTitle('Please mention someone to kiss!')
+		 .setFooter('IAT Bot')
+		 .setTimestamp()
+		 message.channel.send(nomen1)
+	 }else{
+	 
+
+		let {body} = await superagent
+		.get(`https://nekos.life/api/v2/img/kiss`);
+
+		let kissembed = new Discord.RichEmbed()
+		.setColor("RANDOM")
+		.setTitle(`${message.author.username} kissed ${men2.username}! Kawaii!`)
+		.setImage(body.url)
+		.setTimestamp()
+		.setFooter('IAT Bot')
+		message.channel.send(kissembed);
+	 } 
+
+};
+
+module.exports.help = {
+    name: "kiss"
+};
