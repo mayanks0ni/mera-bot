2abb1f5282a21a0f3fba20cecd1beb87d375b0a9
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-02-13 22:37:08 +0530

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -1,10 +1,24 @@
 const Discord = module.require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+if (!message.member.hasPermission("KICK_MEMBERS")) {
+ const noperms = new Discord.RichEmbed()
+.setTitle('You Don't Have Enough Permissions!')
+.setFooter('IAT Bot')
+.setTimestamp()
 
+
+message.channel.send(noperms);
+}else{
 const type1 = args[1]
     const status1 = args.slice(2).join(" ");
    bot.user.setActivity(status1, {type:type1});
+  const statusembed = new Discord.RichEmbed()
+.setTitle(`Updated My Status to ${type1} ${status1}`)
+.setFooter('IAT Bot')
+.setTimestamp()
+message.channel.send(statusembed);
+}
 };
 
 module.exports.help = {
