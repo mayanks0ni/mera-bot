22034f694449da35faeee7450f7fab0be4608799
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-02-13 22:28:20 +0530

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
 const type1 = args[1]
     const status1 = args.slice(2).join(" ");
-   bot.user.setActivity(status1, {type:"LISTENING"});
+   bot.user.setActivity(status1, {type:type1});
 };
 
 module.exports.help = {
