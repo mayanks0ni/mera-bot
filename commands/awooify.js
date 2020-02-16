e3bf4158fe0e9ff64b5a71294e0c515883d21dc1
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-02-16 18:08:24 +0530

    Create awooify.js

---
+++ commands/awooify.js
@@ -0,0 +1,20 @@
+const Discord = module.require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async (bot, message, args) => {
+ let userurl = message.author.displayAvatarURL
+   let {body} = await superagent
+		.get(`https://nekobot.xyz/api/imagegen?type=trash&&url=${userurl}`);
+
+		let awooifyembed = new Discord.RichEmbed()
+		.setColor("RANDOM")
+		.setTitle(`Awooify ${message.author.username}`)
+		.setImage(body.message)
+		.setTimestamp()
+		.setFooter('IAT Bot')
+		message.channel.send(awooifyembed);
+};
+
+module.exports.help = {
+    name: "awooify"
+};
