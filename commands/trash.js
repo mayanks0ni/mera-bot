3d1c76e40101d98c44ec5141a5550da261595f6b
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-02-17 17:39:35 +0530

    Update trash.js

--- commands/trash.js
+++ commands/trash.js
@@ -2,17 +2,18 @@ const Discord = module.require("discord.js");
 const superagent = require("superagent");
 
 module.exports.run = async (bot, message, args) => {
- let userurl = message.author.displayAvatarURL
+ let men = message.mentions.users.first() || message.author
+   let userurl = men.displayAvatarURL
    let {body} = await superagent
 		.get(`https://nekobot.xyz/api/imagegen?type=trash&&url=${userurl}`);
 
-		let awooifyembed = new Discord.RichEmbed()
+		let trashembed = new Discord.RichEmbed()
 		.setColor("RANDOM")
-		.setTitle(`Trash ${message.author.username}!`)
+		.setTitle(`Trash ${men.username}!`)
 		.setImage(body.message)
 		.setTimestamp()
 		.setFooter('IAT Bot')
-		message.channel.send(awooifyembed);
+		message.channel.send(trashembed);
 };
 
 module.exports.help = {
