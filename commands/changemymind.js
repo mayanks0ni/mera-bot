8069e87fab677ba194385215cecb8a4fa873fec1
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-02-17 17:37:07 +0530

    Update changemymind.js

--- commands/changemymind.js
+++ commands/changemymind.js
@@ -4,7 +4,14 @@ const superagent = require("superagent");
 module.exports.run = async (bot, message, args) => {
     
     let text = args.slice(1).join(" ");
-   
+   if(!text){
+		const notext = new Discord.RichEmbed()
+		.setTitle('Please Provide Some Text!')
+		.setFooter('IAT Bot')
+		.setColor(0xff0000)
+		.setTimestamp()
+		message.channel.send(notext);
+	}else{
    let {body} = await superagent
 		.get(`https://nekobot.xyz/api/imagegen?type=changemymind&&text=${text}`);
 
@@ -13,7 +20,7 @@ module.exports.run = async (bot, message, args) => {
 		.setImage(body.message)
 		.setTimestamp()
 		.setFooter('IAT Bot')
-		message.channel.send(tweetembed);
+		message.channel.send(tweetembed);}
 };
 
 module.exports.help = {
