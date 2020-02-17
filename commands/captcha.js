9bb7713d16f2bb374a73c8a43a29912a8bdd87c6
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-02-17 17:36:06 +0530

    Update captcha.js

--- commands/captcha.js
+++ commands/captcha.js
@@ -3,17 +3,26 @@ const superagent = require("superagent");
 
 module.exports.run = async (bot, message, args) => {
     let uname = args[1]
-    let url = message.author.displayAvatarURL
+    if(!uname){
+		const nouname = new Discord.RichEmbed()
+		.setTitle('Please Provide a Username!')
+		.setFooter('IAT Bot')
+		.setColor(0xff0000)
+		.setTimestamp()
+		message.channel.send(nouname);
+	}else{
+   let men = message.mentions.users.first() || message.author
+   let userurl = men.displayAvatarURL
    
    let {body} = await superagent
-		.get(`https://nekobot.xyz/api/imagegen?type=captcha&&username=${uname}&&url=${url}`);
+		.get(`https://nekobot.xyz/api/imagegen?type=captcha&&username=${uname}&&url=${userurl}`);
 
 		let captchaembed = new Discord.RichEmbed()
 		.setColor("RANDOM")
 		.setImage(body.message)
 		.setTimestamp()
 		.setFooter('IAT Bot')
-		message.channel.send(captchaembed);
+		message.channel.send(captchaembed);}
 };
 
 module.exports.help = {
