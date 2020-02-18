4f1a848a05a67749a25da3606600f675909e397a
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-02-18 23:25:36 +0530

    Update inrole.js

--- commands/inrole.js
+++ commands/inrole.js
@@ -3,7 +3,7 @@ const Discord = module.require("discord.js");
 module.exports.run = async (bot, message, args) => {
  let role1 = args.slice(1).join(" ")
       const inroleEmbed = new Discord.RichEmbed()
-            .setTitle('Users with the go4 role:')
+            .setTitle(`Users with the ${role1} role:`)
             .setDescription(message.guild.roles.find(r=>r.name === role1).members.map(m=>m.user.tag).join('\n'))
             .setFooter('IAT Bot')
             .setColor("RANDOM")
