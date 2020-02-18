4374ed17ed4c4dfec39ca0543f20fa66566e4c67
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-02-18 23:18:36 +0530

    Create inrole.js

---
+++ commands/inrole.js
@@ -0,0 +1,15 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+ let role1 = args.slice(1).join(" ")
+      const inroleEmbed = new Discord.RichEmbed()
+            .setTitle('Users with the go4 role:')
+            .setDescription(message.guild.roles.find(r=>r.name === role1).members.map(m=>m.user.tag).join('\n'));
+            .setFooter('IAT Bot');
+            .setTimestamp()
+message.channel.send(inroleEmbed);
+};
+
+module.exports.help = {
+    name: "inrole"
+};
