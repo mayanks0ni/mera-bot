4dc1d48de2b6b253798414fbb34761a58df77169
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-02-18 23:21:29 +0530

    Update inrole.js

--- commands/inrole.js
+++ commands/inrole.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
  let role1 = args.slice(1).join(" ")
       const inroleEmbed = new Discord.RichEmbed()
             .setTitle('Users with the go4 role:')
-            .setDescription(message.guild.roles.find(r=>r.name === role1).members.map(m=>m.user.tag).join('\n'));
+            .setDescription(message.guild.roles.find(r=>r.name === role1).members.map(m=>m.user.tag).join('\n'))
             .setFooter('IAT Bot')
             .setTimestamp()
 message.channel.send(inroleEmbed);
