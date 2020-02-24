51dbd4b1dfdc9f0605c9b85407235932f4b412a9
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-02-24 11:17:01 +0530

    Update del.js

--- commands/del.js
+++ commands/del.js
@@ -4,10 +4,11 @@ module.exports.run = async (bot, message, args) => {
        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
        const nodelperms = new Discord.RichEmbed()
       .setAuthor('You Do Not Have Enough Permissions!!')
+      .setFooter('IAT Bot') 
+      .setTimestamp()
       message.channel.sendEmbed(nodelperms);
      }else{
-       message.delete()
-	message.channel.bulkDelete(args[1]);}
+       message.delete().then(message.channel.bulkDelete(args[1]));}
 };
 
 module.exports.help = {
