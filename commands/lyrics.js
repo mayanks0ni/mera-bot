d218d27af296dea44901daba70e0f89dc34efb8b
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-03-01 17:30:11 +0530

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,31 +1,23 @@
 const Discord = module.require("discord.js");
-const superagent = require("superagent");
+const fetch = require("node-fetch");
 
 module.exports.run = async (bot, message, args) => {
-    let uname = args[1]
-    if(!uname){
-		const nouname = new Discord.RichEmbed()
-		.setTitle('Please Provide The Artists Name!')
-		.setFooter('IAT Bot')
-		.setColor(0xff0000)
-		.setTimestamp()
-		message.channel.send(nouname);
-	}
-    let text = args.slice(2).join(" ");
-	if(!text){
-		const notext = new Discord.RichEmbed()
-		.setTitle('Please Provide The Songs Name!')
-		.setFooter('IAT Bot')
-		.setColor(0xff0000)
-		.setTimestamp()
-		message.channel.send(notext);
-	}else{
-   
-   let {body} = await superagent
-		.get(`https://api.lyrics.ovh/v1/${uname}/${text}`);
-
-
-		message.channel.send(body.lyrics, {split:true});}
+    let name = args.slice(1).join(" ");
+    const url = `https://some-random-api.ml/lyrics?title=${name}`;
+        
+        let res; 
+            res = await fetch(url).then(url => url.json());
+            
+            if(!name){
+                const noname = new Discord.RichEmbed()
+                .setTitle("You Need To Search For A Song!!")
+                .setColor(0xff0000)
+                .setFooter("IAT Bot")
+                .setTimestamp()
+                message.channel.send(noname);
+            }else{
+            message.channel.send(res.lyrics, {split:true})
+            }
 };
 
 module.exports.config = {
